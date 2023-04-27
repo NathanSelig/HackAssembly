750ea6e4df5dd25ba4812b1bf300ef0d876b424c
Nathan Selig <nathanselig@gmail.com>
2023-04-27 12:24:23 -0400

    finished all logical equivilents now workign on all push versions

--- asmToBin/test.asm
+++ asmToBin/test.asm
@@ -1,9 +1,3 @@
     @R0
     A = M - 1
-    D = M
-    A  = A - 1
-    D = D + M
-    A = A + 1
-    M = 0
-    A = A - 1
-    M = D
+    M = !M
--- vmToAsm/assemble.py
+++ vmToAsm/assemble.py
@@ -1,5 +1,23 @@
 """ 
-stack from 255-2047
+stack from  255-1014
+lcl from    1015
+
+push constant i
+    @i
+    D = A
+    @R0
+    A = M
+    M = D
+    @R0
+    M = M + 1
+
+push local i
+    @R1
+    D = M
+    @i
+    A = D + M
+
+
 
 push    [int or var]
     D = input
@@ -14,6 +32,8 @@ pop     [int or var]
     A = M - 1
     output = M
     @R0
+
+#region logic    
 add
     @R0
     A = M - 1
@@ -53,13 +73,21 @@ eq
     D;JEQ
     @R0
     A = M
-    A = A + 1
-    A = 0 
+    A = A - 1
+    M = 0 
+    A = A - 1
+    M = 0
+    @R0
+    M = M - 1
     @next
     0;JMP
     (equal) 
-    A = A + 1
-    A = 1 
+    @R0
+    A = M
+    A = A - 1
+    M = 0 
+    A = A - 1
+    M = 1
     (next)
 gt
     @R0
@@ -68,20 +96,25 @@ gt
     A = A - 1
     D = D - M
     @greater
-    D;JGE
+    D;JGT
     @R0
     A = M
-    A = A + 1
+    A = A - 1
+    M = 0
+    A = A - 1
     M = 0
     @next
     0;JMP
     (greater)
     @R0
     A = M
-    A = A + 1
+    A = A - 1
+    M = 0
+    A = A - 1
     M = 1
     (next)
-    A = A + 1
+    @R0
+    M = M - 1
 lt
     @R0
     A = M - 1
@@ -89,43 +122,47 @@ lt
     A = A - 1
     D = M - D
     @greater
-    D;JGE
+    D;JGT
     @R0
     A = M
-    A = A + 1
+    A = A - 1
+    M = 0
+    A = A - 1
     M = 0
     @next
     0;JMP
     (greater)
     @R0
     A = M
-    A = A + 1
+    A = A - 1
+    M = 0
+    A = A - 1
     M = 1
     (next)
-    A = A + 1
+    @R0
+    M = M - 1
 and
     @R0
     A = M - 1
     D = M
+    M = 0
     A = A - 1
-    D = D & M
+    M = D & M
     @R0
-    A = A + 1
-    D = M 
-    A = A + 1
+    M = M - 1
 or
     @R0
     A = M - 1
     D = M
+    M = 0
     A = A - 1
-    D = D | M
+    M = D | M
     @R0
-    A = A + 1
-    D = M
-    A = A + 1
+    M = M - 1
 not
     @R0
     A = M - 1
     M = !M
-    A = A + 1
+#endregion
+
 """
